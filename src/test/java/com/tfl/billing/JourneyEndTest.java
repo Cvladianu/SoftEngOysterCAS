a39500cb7eb3183cf8e3d4b6ebb0103c8bf75dd3
Cvladianu <zcabcvl@ucl.ac.uk>
2017-11-20 21:39:13 +0000

    created test classes

---
+++ src/test/java/com/tfl/billing/JourneyEndTest.java
@@ -0,0 +1,10 @@
+package com.tfl.billing;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import org.junit.Test;
+/**
+ * Created by cosmi_owugxv5 on 11/20/2017.
+ */
+public class JourneyEndTest {
+}
