516ad8462286623ddb378005f9e031c0c8f83e6d
Cvladianu <zcabcvl@ucl.ac.uk>
2017-11-20 22:27:38 +0000

    same for JourneyStart

--- src/test/java/com/tfl/billing/JourneyStartTest.java
+++ src/test/java/com/tfl/billing/JourneyStartTest.java
@@ -3,8 +3,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 import org.junit.Test;
+
+import java.util.UUID;
+
 /**
  * Created by cosmi_owugxv5 on 11/20/2017.
  */
 public class JourneyStartTest {
+    UUID cardId= UUID.randomUUID();
+    UUID readerId= UUID.randomUUID();
+    JourneyStart journeyStart = new JourneyStart(cardId, readerId);
+
+    @Test
+    public void createdJourneyEnd()
+    {
+        assertThat(journeyStart.cardId(), is(cardId));
+        assertThat(journeyStart.readerId(), is(readerId));
+    }
 }
