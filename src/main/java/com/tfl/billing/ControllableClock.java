7821137094311c80497f32763fad8045d1373c1e
Cvladianu <zcabcvl@ucl.ac.uk>
2017-12-04 17:10:44 +0000

    refactored controllableclock using LocalDateTime

--- src/main/java/com/tfl/billing/ControllableClock.java
+++ src/main/java/com/tfl/billing/ControllableClock.java
@@ -1,15 +1,34 @@
 package com.tfl.billing;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
 
 public class ControllableClock implements Cloackable {
-    private long currentTime = System.currentTimeMillis();
+    private LocalDateTime time;
+    private LocalDateTime now;
+    private long epoch = System.currentTimeMillis();
+    // need to set just the hour, min and seconds, we get the current day and month
+    private int day;
+    private int month;
+    private int year;
+
+    ControllableClock()
+    {
+        now = LocalDateTime.now();
+        month = now.getMonthValue();
+        day = now.getDayOfMonth();
+        year = now.getYear();
+    }
     @Override
     public long currentTimeMillis() {
-        return currentTime;
+        return epoch;
     }
 
     public void setTime(int hour, int minute, int second) {
-        currentTime=1000*(3600*(hour-1)+60*minute+second);
+        time = LocalDateTime.of(year,month,day,hour, minute, second);
+        ZoneId zoneId = ZoneId.systemDefault();
+        epoch = time.atZone(zoneId).toInstant().toEpochMilli();
     }
+
 }
